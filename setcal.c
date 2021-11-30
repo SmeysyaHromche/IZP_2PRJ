e34a3967c68c59a2170bbfe9e8599abc06eb03ac
Humblesaw <94326080+Humblesaw@users.noreply.github.com>
2021-11-30 01:36:41 +0100

    Update setcal.c

--- setcal.c
+++ setcal.c
@@ -3,6 +3,9 @@
 #include <string.h>
 #include <stdbool.h>
 
+#define MAXSTRLEN 30
+#define MAXROW 1000
+
 typedef struct{
     int length;
     char* str;
@@ -31,12 +34,12 @@ typedef struct{
 enum{U, S, R, C};
 enum error{ERR_FOPEN, ERR_ALLOC, ERR_FIRSTLETTER, ERR_MAXSTRLEN, ERR_KEYWORD,
             ERR_ONLYLETTERS, ERR_NOTINUNI, ERR_UNOTFIRST, ERR_TWOUNIS, ERR_FALSEBRACKET,
-            ERR_ELEMNOTINBR, ERR_WRONGNUMOFELEMS, ERR_MISSBR, ERR_MAXROW, ERR_NOFIRSTSPACE};
-
-bool empty(Set set){
-    if(set.length == 0) return true;
-    else return false;
-}
+            ERR_ELEMNOTINBR, ERR_WRONGNUMOFELEMS, ERR_MISSBR, ERR_MAXROW, ERR_NOFIRSTSPACE,
+            ERR_WRONGCMD, ERR_CMDPARAM, ERR_TOOMANYPARAMS, ERR_INVALIDROW, ERR_LACKOFPARAMS,
+            ERR_FUNCROW, ERR_FIRSTCMD, ERR_BRINCMD};
+enum cmdtype{CMD_UNION, CMD_INTERSECT, CMD_MINUS, CMD_SUBSETEQ, CMD_SUBSET, CMD_EQUALS, CMD_EMPTY, CMD_CARD, CMD_COMPLEMENT,
+             CMD_REFLEXIVE, CMD_SYMMETRIC, CMD_ANTISYMMETRIC, CMD_TRANSITIVE, CMD_FUNCTION, CMD_DOMAIN, CMD_CODOMAIN,
+             CMD_INJECTIVE, CMD_SURJECTIVE, CMD_BIJECTIVE};
 
 void freeAll(String* str, Set* set, Relation* rel, Position* pos, FILE* fp){
     if(str != NULL) free(str->str);
@@ -99,19 +102,55 @@ void error(int type, String* str, Set* set, Relation* rel, Position* pos, FILE* 
         break;
 
         case ERR_WRONGNUMOFELEMS:
-            fprintf(stderr, "Number of expressions in one element of relation has to be 2\n");
+            fprintf(stderr, "Number of expressions in one element of a relation has to be 2\n");
         break;
 
         case ERR_MISSBR:
-            fprintf(stderr, "There is a missing bracket at the end of relation\n");
+            fprintf(stderr, "There is a missing bracket at the end of a relation\n");
         break;
 
         case ERR_MAXROW:
-            fprintf(stderr, "Maximum amount of rows in file has been reached [%d]\n", MAXROW);
+            fprintf(stderr, "Maximum amount of rows in a file has been reached [%d]\n", MAXROW);
         break;
 
         case ERR_NOFIRSTSPACE:
             fprintf(stderr, "There has to be space between command | U | S | R | C | and elements or other commands\n");
+        break;
+
+        case ERR_WRONGCMD:
+            fprintf(stderr, "Unknown type of command\n");
+        break;
+
+        case ERR_CMDPARAM:
+            fprintf(stderr, "Parameters of commands can only be numbers of existing rows\n");
+        break;
+
+        case ERR_TOOMANYPARAMS:
+            fprintf(stderr, "Command contains too many parameters\n");
+        break;
+
+        case ERR_INVALIDROW:
+            fprintf(stderr, "Parameter of a command is an invalid row\n");
+        break;
+
+        case ERR_LACKOFPARAMS:
+            fprintf(stderr, "Command-row is lacking a command or some parameters\n");
+        break;
+
+        case ERR_FUNCROW:
+            fprintf(stderr, "You cannot combine commands with wrong rows\n");
+        break;
+
+        case ERR_FIRSTCMD:
+            fprintf(stderr, "You are not allowed to declare sets or relations after a command\n");
+        break;
+
+        case ERR_BRINCMD:
+            fprintf(stderr, "Brackets are not allowed when declaring a command\n");
+        break;
+
+        default:
+            fprintf(stderr, "Error!\n");
         break;
     }
 
@@ -172,6 +211,30 @@ Position* reallocPos(Position* pos, int numOfSets, int numOfRels){ //allocates m
     return pos; //could be null
 }
 
+int checkCmd(char* value){
+    if(!strcmp(value, "empty")) return CMD_EMPTY;
+    if(!strcmp(value, "card")) return CMD_CARD;
+    if(!strcmp(value, "complement")) return CMD_COMPLEMENT;
+    if(!strcmp(value, "union")) return CMD_UNION;
+    if(!strcmp(value, "intersect")) return CMD_INTERSECT;
+    if(!strcmp(value, "minus")) return CMD_MINUS;
+    if(!strcmp(value, "subseteq")) return CMD_SUBSETEQ;
+    if(!strcmp(value, "subset")) return CMD_SUBSET;
+    if(!strcmp(value, "equals")) return CMD_EQUALS;
+    if(!strcmp(value, "reflexive")) return CMD_REFLEXIVE;
+    if(!strcmp(value, "symmetric")) return CMD_SYMMETRIC;
+    if(!strcmp(value, "antisymmetric")) return CMD_ANTISYMMETRIC;
+    if(!strcmp(value, "transitive")) return CMD_TRANSITIVE;
+    if(!strcmp(value, "function")) return CMD_FUNCTION;
+    if(!strcmp(value, "domain")) return CMD_DOMAIN;
+    if(!strcmp(value, "codomain")) return CMD_CODOMAIN;
+    if(!strcmp(value, "injective")) return CMD_INJECTIVE;
+    if(!strcmp(value, "surjective")) return CMD_SURJECTIVE;
+    if(!strcmp(value, "bijective")) return CMD_BIJECTIVE;
+
+    return -1;
+}
+
 bool checkStr(char* value, String* str, int numOfElems){
     if(!strcmp(value, "U") ||
        !strcmp(value, "S") ||
@@ -179,25 +242,7 @@ bool checkStr(char* value, String* str, int numOfElems){
        !strcmp(value, "C") ||
        !strcmp(value, "true") ||
        !strcmp(value, "false") ||
-       !strcmp(value, "empty") ||
-       !strcmp(value, "card") ||
-       !strcmp(value, "complement") ||
-       !strcmp(value, "union") ||
-       !strcmp(value, "intersect") ||
-       !strcmp(value, "minus") ||
-       !strcmp(value, "subseteq") ||
-       !strcmp(value, "subset") ||
-       !strcmp(value, "equals") ||
-       !strcmp(value, "reflexive") ||
-       !strcmp(value, "symmetric") ||
-       !strcmp(value, "antisymmetric") ||
-       !strcmp(value, "transitive") ||
-       !strcmp(value, "function") ||
-       !strcmp(value, "domain") ||
-       !strcmp(value, "codomain") ||
-       !strcmp(value, "injective") ||
-       !strcmp(value, "surjective") ||
-       !strcmp(value, "bijective"))
+       (checkCmd(value) > -1))
             return true;
 
     if(str != NULL){
@@ -261,6 +306,229 @@ bool checkRelStr2(Set* set, Relation* rel, char* value, int numOfRels, int numOf
             return true;
     }
     return false;
+}
+
+void outputbool(bool b){
+    if(b) printf("true\n");
+    else printf("false\n");
+}
+
+void outputnum(int i){
+    printf("%d\n", i);
+}
+
+void outputset(char* value){
+    printf("%s ", value);
+}
+
+void empty(Set set){
+    outputbool(set.length == 0);
+}
+
+void card(Set set){
+    outputnum(set.length);
+}
+
+void complement(Set set, Set uni){
+    bool isIn = false;
+    printf("S ");
+    for(int i = 0; i < uni.length; i++){
+        for(int j = 0; j < set.length; j++){
+            if(uni.set[i]->str == set.set[j]->str){
+                isIn = true;
+                break;
+            }
+        }
+        if(!isIn)
+            outputset(uni.set[i]->str);
+        else
+            isIn = false;
+    }
+    printf("\n");
+}
+
+void _union(Set set1, Set set2){
+    bool isIn = false;
+    bool toWrite = true;
+    printf("S ");
+    for(int i = 0; i < set1.length; i++){
+        for(int j = 0; j < set2.length; j++){
+            if(toWrite) outputset(set2.set[j]->str);
+
+            if(set1.set[i]->str == set2.set[j]->str){
+                isIn = true;
+                if(!toWrite) break;
+            }
+        }
+        toWrite = false;
+        if(!isIn)
+            outputset(set1.set[i]->str);
+        else
+            isIn = false;
+    }
+    printf("\n");
+}
+
+void intersect(Set set1, Set set2){
+    printf("S ");
+    for(int i = 0; i < set1.length; i++){
+        for(int j = 0; j < set2.length; j++){
+            if(set1.set[i]->str == set2.set[j]->str){
+                outputset(set1.set[i]->str);
+                break;
+            }
+        }
+    }
+    printf("\n");
+}
+
+void _minus(Set set1, Set set2){
+    complement(set2, set1);
+}
+
+void subseteq(Set set1, Set set2){
+    int count = 0;
+    for(int i = 0; i < set1.length; i++){
+        for(int j = 0; j < set2.length; j++){
+            if(set1.set[i]->str == set2.set[j]->str){
+                count++;
+                break;
+            }
+        }
+    }
+    if(count == set1.length)
+        outputbool(true);
+    else
+        outputbool(false);
+}
+
+void equals(Set set1, Set set2){
+    if(set1.length != set2.length){
+        outputbool(false);
+        return;
+    }
+    subseteq(set1, set2);
+}
+
+void subset(Set set1, Set set2){
+    if(set1.length == set2.length){
+        outputbool(false);
+        return;
+    }
+    subseteq(set1, set2);
+}
+
+void reflexive(){
+    
+}
+
+void callFunc(int type, int row1, int row2, String* str, Set* set, Relation* rel, Position* pos, FILE* fp){
+
+    if(row2 != 0) {
+        if(pos[row1 - 1].isSet && pos[row2 - 1].isSet){
+            switch(type){
+                case CMD_UNION:
+                    _union(set[pos[row1 - 1].pos], set[pos[row2 - 1].pos]);
+                break;
+
+                case CMD_INTERSECT:
+                    intersect(set[pos[row1 - 1].pos], set[pos[row2 - 1].pos]);
+                break;
+
+                case CMD_MINUS:
+                    _minus(set[pos[row1 - 1].pos], set[pos[row2 - 1].pos]);
+                break;
+
+                case CMD_SUBSETEQ:
+                    subseteq(set[pos[row1 - 1].pos], set[pos[row2 - 1].pos]);
+                break;
+
+                case CMD_SUBSET:
+                    subset(set[pos[row1 - 1].pos], set[pos[row2 - 1].pos]);
+                break;
+
+                case CMD_EQUALS:
+                    equals(set[pos[row1 - 1].pos], set[pos[row2 - 1].pos]);
+                break;
+
+                default:
+                    error(ERR_FUNCROW, str, set, rel, pos, fp);
+                break;
+            }
+
+        } else {
+           error(ERR_FUNCROW, str, set, rel, pos, fp);
+        }
+
+    } else {
+        if(pos[row1 - 1].isSet){
+            switch(type){
+                case CMD_EMPTY:
+                    empty(set[pos[row1 - 1].pos]);
+                break;
+
+                case CMD_CARD:
+                    card(set[pos[row1 - 1].pos]);
+                break;
+
+                case CMD_COMPLEMENT:
+                    complement(set[pos[row1 - 1].pos], set[0]);
+                break;
+
+                default:
+                    error(ERR_FUNCROW, str, set, rel, pos, fp);
+                break;
+            }
+
+        } else {
+            switch(type){
+                case CMD_REFLEXIVE:
+                    //Reflexive(rel[pos[row1 - 1].pos]);
+                break;
+
+                case CMD_SYMMETRIC:
+                    //Symmetric(rel[pos[row1 - 1].pos]);
+                break;
+
+                case CMD_ANTISYMMETRIC:
+                    //Antisymmetric(rel[pos[row1 - 1].pos]);
+                break;
+
+                case CMD_TRANSITIVE:
+                    //Transitive(rel[pos[row1 - 1].pos]);
+                break;
+
+                case CMD_FUNCTION:
+                    //Function(rel[pos[row1 - 1].pos]);
+                break;
+
+                case CMD_DOMAIN:
+                    //Domain(rel[pos[row1 - 1].pos]);
+                break;
+
+                case CMD_CODOMAIN:
+                    //Codomain(rel[pos[row1 - 1].pos]);
+                break;
+
+                case CMD_INJECTIVE:
+                    //Injective(rel[pos[row1 - 1].pos]);
+                break;
+
+                case CMD_SURJECTIVE:
+                    //Surjective(rel[pos[row1 - 1].pos]);
+                break;
+
+                case CMD_BIJECTIVE:
+                    //Bijective(rel[pos[row1 - 1].pos]);
+                break;
+
+                default:
+                    error(ERR_FUNCROW, str, set, rel, pos, fp);
+                break;
+            }
+        }
+    }
+    return;
 }
 
 bool hasElem(Element *elem, Relation *rel) {
